37314ae6894b1cbe6b0867e3b2066a2a9a8c0718
Tony H <babayandbabay@gmail.com>
2020-10-30 21:59:11 +0200

    added express for Heroku port opening

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import { launch } from "./bot"
 import db from './db'
 import './extensions/date.extension'
 import "./extensions/string.extension"
+import express from 'express'
+// Create a new express app instance
+const app: express.Application = express();
+
+app.get(`/`, function (req, res) {
+    res.send(`Hello World!`);
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`App is listening on port 3000!`);
+});
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
